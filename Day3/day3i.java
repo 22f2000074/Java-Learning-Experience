7aa73009f35dfb690f89f04259d034bff46c56ae
Vaibhav Madan <workss.vaibhav@gmail.com>
2025-03-11 18:36:26 +0530

    "Day-3"

--- Day3/day3i.java
+++ Day3/day3i.java
@@ -27,11 +27,40 @@ public class day3i {
       *****
     */
     public static void pattern2(){
-        
+        for (int i=1;i<=4;i++){
+            for (int j=1;j<=5;j++){
+                if(i==1 || j==1 || i==4 || j==5){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    /*
+        Pattern 3 Half Pyramid
+        *
+        **
+        ***
+        ****
+    */
+    public static void pattern3(){
+        for(int i=0;i<4;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 
     public static void main(){
         System.out.println("Printing Pattern 1");
         pattern1();
+        System.out.println("\nPrinting Pattern 2");
+        pattern2();
+        System.out.println("\nPrinting Pattern 3");
+        pattern3();
     }
 }
